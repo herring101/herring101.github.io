64023e2c181b42fe2c28a14f371f64362bda3161
herring101 <81513223+herring101@users.noreply.github.com>
2025-02-05 00:54:02 +0000

    Deploying to gh-pages from @ herring101/herring101.github.io@860ceded8ef335398e2d13f74de65d0ed445dbc7 🚀

---
+++ _next/static/chunks/app/research/[slug]/page-cd8b4146143c6d65.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[797,214,169,53],{6743:function(n,e,u){Promise.resolve().then(u.t.bind(u,9605,23))}},function(n){n.O(0,[605,653,848,744],function(){return n(n.s=6743)}),_N_E=n.O()}]);
